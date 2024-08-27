00fc3b257aae14350b3409bec9202d61ce7d497c
yhmunna1 <yh.munna.barisal@gmail.com>
2024-08-27 19:14:32 +0600

    loading problem fixed and spinner added

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -11,22 +11,27 @@ export const AuthContext = createContext(null);
 
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   const createUser = (email, password) => {
+    setLoading(true);
     return createUserWithEmailAndPassword(auth, email, password);
   };
 
   const signInUser = (email, password) => {
+    setLoading(true);
     return signInWithEmailAndPassword(auth, email, password);
   };
 
   const logOut = () => {
+    setLoading(true);
     signOut(auth);
   };
 
   useEffect(() => {
     const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
+      setLoading(false);
       console.log("current user:", currentUser);
     });
     return () => {
@@ -34,7 +39,7 @@ const AuthProvider = ({ children }) => {
     };
   }, []);
 
-  const authInfo = { user, createUser, signInUser, logOut };
+  const authInfo = { user, loading, createUser, signInUser, logOut };
 
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -3,7 +3,10 @@ import { AuthContext } from "../provider/AuthProvider";
 import { Navigate } from "react-router-dom";
 
 const PrivateRoute = ({ children }) => {
-  const { user } = useContext(AuthContext);
+  const { user, loading } = useContext(AuthContext);
+  if (loading) {
+    return <span className="loading loading-spinner loading-lg"></span>;
+  }
   if (user) {
     return children;
   }
